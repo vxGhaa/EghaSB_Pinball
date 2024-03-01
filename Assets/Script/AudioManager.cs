00f1d54d26e3a92c6df0ddbcbb050022f7a62965
vxGhaa <cloudghaa@gmail.com>
2024-03-01 17:27:13 +0700

    Backup
    Full Backup

---
+++ Assets/Script/AudioManager.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    //audio
+    public AudioSource bgmAudioSource;
+    public GameObject sfxAudioSource;
+
+    private void Start()
+    {
+        PlayBGM();
+    }
+
+    private void PlayBGM()
+    {
+        bgmAudioSource.Play();
+    }
+
+    // Menggunakan UnityEngine.Vector3 dan UnityEngine.Quaternion untuk menghindari ambiguitas
+    public void PlaySFX(Vector3 spawnPosition)
+    {
+        Instantiate(sfxAudioSource, spawnPosition, Quaternion.identity);
+    }
+}
